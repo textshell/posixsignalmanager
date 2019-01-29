cf23247b063633d8e81e5c51f2fdedfbdc428afe
Martin Hostettler <textshell@uchuujin.de>
2019-01-29 08:08:52 +0100

    tests: Don't assume that core dumping is disabled.

--- tests/catch_main.cpp
+++ tests/catch_main.cpp
@@ -1,3 +1,13 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #define CATCH_CONFIG_NO_POSIX_SIGNALS
 #include "catch.hpp"
+
+#include <sys/resource.h>
+
+int main( int argc, char* argv[] ) {
+    rlimit lim;
+    getrlimit(RLIMIT_CORE, &lim);
+    lim.rlim_cur = 0;
+    setrlimit(RLIMIT_CORE, &lim);
+    return Catch::Session().run( argc, argv );
+}
